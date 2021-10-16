e840e98916a68f67d970b540e5a33f171158b14a
gtorres17 <gtorres89@outlook.com.br>
2021-10-15 21:14:05 -0300

    Exemplo projeto base backend (Spring + Kotlin + Gradle)

---
+++ project/api/src/test/kotlin/com/fatec/baltacompras/BaltaComprasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fatec.baltacompras
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaltaComprasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
