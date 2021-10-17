0587593233f0e4d02d9d60f18a80403fd6aa69bc
gtorres17 <gtorres89@outlook.com.br>
2021-10-17 14:01:15 -0300

    Arquitetura inicial

---
+++ project/ui/src/app/shared/components/index.ts
@@ -0,0 +1,5 @@
+// import { ButtonBComponent } from './button-b/button-b.component';
+// export const components: any[] = [ButtonAComponent, ButtonBComponent];
+// export * from './button-a/button-a.component';
+
+export const components: any[] = []
