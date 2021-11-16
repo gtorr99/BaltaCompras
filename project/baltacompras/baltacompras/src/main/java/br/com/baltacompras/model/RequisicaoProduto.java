4008ee81e1624dab53c425bdee8753f6062bcbd1
JhowFerri <jhowvore@gmail.com>
2021-11-16 20:12:22 -0300

    remoção do jsonignore da classe requisicaoproduto

--- project/baltacompras/baltacompras/src/main/java/br/com/baltacompras/model/RequisicaoProduto.java
+++ project/baltacompras/baltacompras/src/main/java/br/com/baltacompras/model/RequisicaoProduto.java
@@ -1,8 +1,14 @@
 package br.com.baltacompras.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "requisicao_produto")
@@ -17,7 +23,6 @@ public class RequisicaoProduto {
     @JoinColumn(name = "id_requisicao")
     private Requisicao requisicao;
 
-    @JsonIgnore
     @ManyToOne
     @MapsId("idProduto")
     @JoinColumn(name = "id_produto")
